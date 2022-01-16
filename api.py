daf1ddf9e9b306ff1e8788ad4c77dd162e6e5155
Guillaume <guillaume.dutech@gmail.com>
2022-01-16 22:35:58 +0100

    Add get_predict_simu

--- api.py
+++ api.py
@@ -20,14 +20,6 @@ model = open('pipe_model.joblib','rb')
 clf = joblib.load(model)
 
 
-# id_client = 104460
-
-
-# resp = df[df['SK_ID_CURR']==int(id_client)]
-
-# print(resp)
-
-
 @app.get('/')
 async def index() : 
     return {"text" : "Hello World!"}
@@ -39,7 +31,7 @@ async def get_predict(id_client) :
 
     target = clf.predict(resp)[0]
 
-    score = clf.predict_proba(resp)[0][0]
+    score = max(clf.predict_proba(resp)[0])
     
     if int(target) == 1 : 
         prediction = "Faulter"
@@ -48,6 +40,29 @@ async def get_predict(id_client) :
         
         
     return {"id" : id_client, "score" : str(score), "prediction" : prediction}
+
+@app.get('/predictions')
+async def get_predict_simu(id_client,new_credit_amt,new_credit_ann,new_income_percent,new_credit_term) :
+    
+    resp = df[df['SK_ID_CURR']==int(id_client)]
+    
+    resp["AMT_CREDIT"] = new_credit_amt
+    resp["AMT_ANNUITY"] = new_credit_ann
+    resp["CREDIT_INCOME_PERCENT"] = new_income_percent
+    resp["CREDIT_TERM"] = new_credit_term
+    
+    target = clf.predict(resp)[0]
+
+    score = max(clf.predict_proba(resp)[0])
+    
+    if int(target) == 1 : 
+        prediction = "Faulter"
+    else : 
+        prediction = "Non Faulter"
+        
+        
+    return {"id" : id_client, "score" : str(score), "prediction" : prediction}
+    
 
 if __name__ == '__main__' : 
     uvicorn.run(app, host="127.0.0.1",port=8000)
